fd123b4d75852a75197e93af291040bdd38e3fac
gmdep003 <95266030+gmdep003@users.noreply.github.com>
2022-11-12 21:06:51 -0500

    Update ButtonWindObs.cs

--- Assets/CMSC425/WxProb/Code/Observers/ButtonWindObs.cs
+++ Assets/CMSC425/WxProb/Code/Observers/ButtonWindObs.cs
@@ -13,11 +13,23 @@ namespace Wx
         void Start()
         {
             // Add code to obtain reports of state changes.
+            tmp = GetComponent<TextMeshPro>();
+            wind.ReportState += ReportState;
+            id = wind.airportID;
         }
 
         private void ReportState(bool isSimulated)
         {
             // Add code to manage reports of state changes.
+            string state = "";
+
+            if(isSimulated){
+                state = "sim";
+            }else {
+                state = "net";
+            }
+
+            tmp.text = id + "\n" + state;
         }
     }
 }
